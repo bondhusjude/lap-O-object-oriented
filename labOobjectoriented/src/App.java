accde1979682631d2cfebc0f65ff035441caf95c
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2025-02-27 15:40:13 -0600

    modified:   labOobjectoriented/src/App.java

--- labOobjectoriented/src/App.java
+++ labOobjectoriented/src/App.java
@@ -10,6 +10,16 @@ public class App {
         data.add( 3 );
         data.add( 32 );
         Collections.sort( data );
+        
+        int previous_num = data.get(0);
+        for ( Integer n : data ) {
+            if ( n >= previous_num ) {
+                previous_num = n;
+            } else {
+                System.out.println("Data is not sorted");
+                break;
+            }
+        }
 
          for ( Integer n : data ) {
      System.out.println( "Item: " + n );
